bd3cca8c08b074dbbc3144cddb4c18ad6518afce
7deathmaster7 <pablo.diamante@hotmail.com>
2020-03-18 17:47:27 -0300

    Add files via upload

---
+++ example.py
@@ -0,0 +1,45 @@
+import argparse
+
+def Func (valor):
+	if valor == 1 or valor == 0:
+		result = valor
+		return result
+	else:
+		result = valor * Func(valor - 1)
+		return result
+
+def Funciter(valor):
+	result = 1
+	for x in range(valor,1,-1):
+		result *= x
+	return result 
+
+def Funciter2(valor):
+	result = 1
+	while 1 < valor:
+		result *= valor
+		valor = valor -1
+	return result 
+
+def main(valor,metodo):
+	print('************************************Fatorial************************************\n\n Digite 1 para cálculo com fatorial recursivo;\n 2 para cálculo fatorial com for;\n ou 3 para cálculo fatorial com while\n\n\n')
+	result = 0
+
+	x = valor
+	if metodo == 1:
+			
+		result = Func(x)
+		print('O valor do fatorial é: %d \n\n' % result)
+	elif metodo ==2:
+		result = Funciter(x)
+		print('O valor do fatorial iterativo é: %d \n\n' % result)
+	else:
+		result = Funciter2(x)
+		print('O valor do fatorial iterativo com while é: %d \n\n' % result)
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser()
+	parser.add_argument("valor",type=int,help = "entrada de dados para calculo do fatorial")
+	parser.add_argument("metodo",type= int,help = "método")
+	args = parser.parse_args()
+	main(args.valor,args.metodo)
